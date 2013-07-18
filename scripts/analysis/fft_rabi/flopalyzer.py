cc98a4fb7d0e445f862c7cfe63629a42aa4eab8e
Dylan Gorman <dgorman@berkeley.edu>
2013-07-18 12:20:56 -0700

    Fixed some flopalyzer bug

--- scripts/analysis/fft_rabi/flopalyzer.py
+++ scripts/analysis/fft_rabi/flopalyzer.py
@@ -18,8 +18,8 @@ class Flopalyzer:
         self.date = date
         self.datasetname = datasetname
         data = self.get_data()
-        self.time = self.data[:,0]
-        self.excitation = self.data[:,1]
+        self.time = data[:,0]
+        self.excitation = data[:,1]
         self.freq, self.pos_freq_components = self.do_fft()
     
     def get_data(self):
@@ -34,26 +34,29 @@ class Flopalyzer:
 
 
         sideband_selection = dv.get_parameter('RabiFlopping.sideband_selection')
-        
+        print sideband_selection
         try:
             sideband_number = sideband_selection.index(1)
+            print sideband_number
         except:
             print "Not a first order blue sideband! I can't figure out eta for you!"
             sideband_number = None
 
         if sideband_number is not None:
-            sideband_list = ['TrapFrequencies.rf_drive_frequency',
-                             'TrapFrequencies.axial_frequency', 
+            sideband_list = [                         
                              'TrapFrequencies.radial_frequency_1',
-                             'TrapFrequencies.radial_frequency_2']
+                             'TrapFrequencies.radial_frequency_2',
+                             'TrapFrequencies.axial_frequency', 
+                             'TrapFrequencies.rf_drive_frequency']
 
             sideband = sideband_list[sideband_number]
+            print sideband
             self.trap_frequency = dv.get_parameter(sideband)
-        
+            print dv.get_parameter(sideband)
         return data.asarray
 
     def do_fft(self):
-        tf = self.time[-1]
+        tf = self.time[-1]*1e-6
         n = len(self.excitation)
         x = fft.fft(self.excitation)
         pos_freq_components = x[1:n/2]
@@ -72,26 +75,23 @@ class Flopalyzer:
         '''
         May need a better peak detection algorithm in future
         '''
-        try:
-            wavelength = U.WithUnit(729.0, 'nm')
-            m = 40*amu
-            chi =  2*np.pi/wavelength['m']*np.sqrt(hbar['J*s']/(2.*m['kg']*2.*np.pi*self.trap_frequency['Hz']))
-            freq_data = list(np.abs(self.pos_freq_components))        
-            maxElemIndex = freq_data.index(max(freq_data))
-            peakFreq = freq[maxElemIndex]*1e3 # freq in Hz
-            theta = U.WithUnit( np.arccos(peakFreq/(chi*bareRabi['Hz']))*180.0/np.pi , 'deg' )
-            return theta
-        except:
-            print "I already warned you that I can't compute your Lamb-Dicke parameter. You chose not to listen. Now look what you've done."
-            return None
+        
+        wavelength = U.WithUnit(729.0, 'nm')
+        m = 40*amu
+        chi =  2*np.pi/wavelength['m']*np.sqrt(hbar['J*s']/(2.*m['kg']*2.*np.pi*self.trap_frequency['Hz']))
+        freq_data = list(np.abs(self.pos_freq_components))        
+        maxElemIndex = freq_data.index(max(freq_data))
+        peakFreq = self.freq[maxElemIndex]*1e3 # freq in Hz
+        theta = U.WithUnit( np.arccos(peakFreq/(chi*bareRabi['Hz']))*180.0/np.pi , 'deg' )
+        return theta
 
 if __name__ == "__main__":
 
     bareRabi = U.WithUnit(56136.0, 'Hz')
 
-    date = '2013Jul15'
-    datasetname = '1549_31'
-
+    date = '2013Jul16'
+    datasetname = '0054_02'
+    #datasetname = '0056_00'
     f = Flopalyzer(date, datasetname)
     print "Projection: " + str(f.compute_projection(bareRabi))
 
