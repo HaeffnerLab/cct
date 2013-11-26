4a847c0124c926160543996a4f8e28c31897a416
Dylan Gorman <dgorman@berkeley.edu>
2013-11-25 23:14:46 -0800

    Pulsed heating added

---
+++ scripts/PulseSequences/advanceDACsShuttle_to.py
@@ -0,0 +1,15 @@
+from common.okfpgaservers.pulser.pulse_sequences.pulse_sequence import pulse_sequence
+from labrad.units import WithUnit
+
+class advance_DACs_shuttle(pulse_sequence):
+	required_parameters = [
+							('advanceDACs', 'pulse_length'),
+							('advanceDACs', 'times'),
+	]
+
+	def sequence( self ):
+		pl = self.parameters.advanceDACs.pulse_length
+		times = self.parameters.advanceDACs.times
+		print 'times', times
+		for t in times:
+			self.addTTL('adv', WithUnit(t, 's'), pl)
---
+++ scripts/PulseSequences/subsequences/PulsedHeating.py
@@ -0,0 +1,21 @@
+from common.okfpgaservers.pulser.pulse_sequences.pulse_sequence import pulse_sequence
+    
+class pulsed_heating(pulse_sequence):
+
+	required_parameters = [
+                  ('PulsedHeating','pulsed_heating_duration'),
+                  ('PulsedHeating','pulsed_heating_amplitude_397'),
+                  ('PulsedHeating','pulsed_heating_frequency_397'),
+                  ('DopplerCooling','doppler_cooling_frequency_866'), 
+                  ('DopplerCooling','doppler_cooling_amplitude_866')
+                  ]
+
+	def sequence(self):
+		ph = self.parameters.PulsedHeating
+		dc = self.parameters.DopplerCooling
+		dur = ph.pulsed_heating_duration
+
+		self.end = self.start + dur
+		self.addTTL('397mod', self.start, dur)
+		self.addDDS ('397',self.start, dur, ph.pulsed_heating_frequency_397, ph.pulsed_heating_amplitude_397)
+		self.addDDS ('866',self.start, dur, dc.doppler_cooling_frequency_866, dc.doppler_cooling_amplitude_866)
